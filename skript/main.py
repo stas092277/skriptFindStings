272dfb3f4d8052425a040cbd90194e2dd7f30952
Stanislav Chebikin <47776095+stas092277@users.noreply.github.com>
2021-09-03 16:57:39 +0300

    new feature
    Add feature of finding dependents

--- skript/main.py
+++ skript/main.py
@@ -3,6 +3,8 @@ import re
 upiRe = '(.*) = UserPortIn'
 upoRe = '(.*) = UserPortOut'
 
+dependencies = {}
+
 
 def writingFile(nameFile, lines):
     with open(nameFile, "w") as file:
@@ -10,7 +12,7 @@ def writingFile(nameFile, lines):
             file.write(line + '\n')
 
 
-def find():
+def find_names():
     with open('default_simple_python_block.py', 'r') as file:
         lines = file.readlines()
 
@@ -18,12 +20,12 @@ def find():
     upo_set = set()
     both_set = set()
     for line in lines:
+        if line[0] == '#':
+            continue
         if re.match(upiRe, line) is not None:
-            line = line.replace('#', "").replace(" ", "").removesuffix("=UserPortIn()\n")
-            upi_set.add(line)
+            upi_set.add(str(re.findall(r'(\w+) = UserPortIn', line)[0]))
         if re.match(upoRe, line) is not None:
-            line = line.replace("type=np.ndarray", "").replace('#', "").replace(" ", "").removesuffix("=UserPortOut()\n")
-            upo_set.add(line)
+            upo_set.add(str(re.findall(r'(\w+) = UserPortOut', line)[0]))
 
     both_set.update(upi_set, upo_set)
 
@@ -31,10 +33,41 @@ def find():
     upo_set = sorted(upo_set)
     both_set = sorted(both_set)
 
+
     writingFile("UserPortIn.txt", upi_set)
     writingFile("UserPortOut.txt", upo_set)
     writingFile("UserPortInAndOut.txt", both_set)
 
 
+def find_dependensis():
+    with open('default_simple_python_block.py', 'r') as file:
+        text = file.read()
+
+    text = text.replace('\n', '')
+    classes = text.split('class')
+    classes.pop(0)
+
+    for tmp in classes:
+        ins = re.findall(r'(\w+) = UserPortIn', tmp)
+        out = re.findall(r'(\w+) = UserPortOut', tmp)
+        dependencies[out[0]] = ins
+
+
+
+def get_all_input_params_for_out(name_out_param: str):
+    return dependencies[name_out_param]
+
+
+def get_all_output_params_for_input(name_int_param: str):
+    ans = []
+    for key in dependencies.keys():
+        if name_int_param in dependencies[key]:
+            ans.append(key)
+    return(ans)
+
+
 if __name__ == '__main__':
-    find()
+    find_names()
+    find_dependensis()
+    print(get_all_input_params_for_out('дальность_обнаружения'))
+    print(get_all_output_params_for_input('КУ_антенны'))

5a7ccfc9ee6b378ff793f2fa28f5967475e57863
Stanislav Chebikin <47776095+stas092277@users.noreply.github.com>
2021-12-28 16:43:30 +0400

    Add class ParamsManager
    Add class ParamsManager

--- skript/main.py
+++ skript/main.py
@@ -8,7 +8,53 @@ all_inputs = set()
 using_params = set()
 
 
-def writingFile(nameFile, lines):
+class ParamsManager:
+    def __init__(self, path="default_simple_python_block.py"):
+        self.path = path
+        self.dependencies = {}
+        self.all_inputs = set()
+        self.using_params = set()
+
+    def find_dependencies(self):
+        with open(self.path, 'r') as file:
+            text = file.read()
+
+        text = text.replace('\n', '')
+        classes = text.split('class')
+        classes.pop(0)
+
+        for tmp in classes:
+            ins = re.findall(r'(\w+) = UserPortIn', tmp)
+            out = re.findall(r'(\w+) = UserPortOut', tmp)
+            dependencies[out[0]] = ins
+            for tmp in ins:
+                all_inputs.add(tmp)
+
+    def get_all_input_params_for_out(self, name_out_param: str):
+        return [param for param in dependencies[name_out_param] if param not in using_params]
+
+    def get_all_output_params_for_input(self, name_int_param: str):
+        ans = []
+        for key in dependencies.keys():
+            if name_int_param in dependencies[key]:
+                ans.append(key)
+        return ans
+
+    def get_info_about_input_param(self, name_int_param: str):
+        if name_int_param in all_inputs:
+
+            out_puts = self.get_all_output_params_for_input(name_int_param)
+            for out_put in out_puts:
+                needed_input = self.get_all_input_params_for_out(out_put)
+                if len(needed_input) == 0:
+                    print(f'-{out_put}.')
+                else:
+                    print(f'-{out_put}. Не хватает: {self.get_all_input_params_for_out(out_put)}')
+        else:
+            raise ValueError("Нет такого параметра")
+
+
+def writing_file(nameFile, lines):
     with open(nameFile, "w") as file:
         for line in lines:
             file.write(line + '\n')
@@ -35,62 +81,19 @@ def find_names():
     upo_set = sorted(upo_set)
     both_set = sorted(both_set)
 
-    writingFile("UserPortIn.txt", upi_set)
-    writingFile("UserPortOut.txt", upo_set)
-    writingFile("UserPortInAndOut.txt", both_set)
-
-
-def find_dependensis():
-    with open('default_simple_python_block.py', 'r') as file:
-        text = file.read()
-
-    text = text.replace('\n', '')
-    classes = text.split('class')
-    classes.pop(0)
-
-    for tmp in classes:
-        ins = re.findall(r'(\w+) = UserPortIn', tmp)
-        out = re.findall(r'(\w+) = UserPortOut', tmp)
-        dependencies[out[0]] = ins
-        for tmp in ins:
-            all_inputs.add(tmp)
-
-
-def get_all_input_params_for_out(name_out_param: str):
-    return [param for param in dependencies[name_out_param] if param not in using_params]
-
-
-def get_all_output_params_for_input(name_int_param: str):
-    ans = []
-    for key in dependencies.keys():
-        if name_int_param in dependencies[key]:
-            ans.append(key)
-    return (ans)
-
-
-def get_info_about_input_param(name_int_param: str):
-    if name_int_param in all_inputs:
-
-        out_puts = get_all_output_params_for_input(name_int_param)
-        for out_put in out_puts:
-            needed_input = get_all_input_params_for_out(out_put)
-            if len(needed_input) == 0:
-                print(f'-{out_put}.')
-            else:
-                print(f'-{out_put}. Не хватает: {get_all_input_params_for_out(out_put)}')
-    else:
-        raise ValueError("Нет такого параметра")
+    writing_file("UserPortIn.txt", upi_set)
+    writing_file("UserPortOut.txt", upo_set)
+    writing_file("UserPortInAndOut.txt", both_set)
 
 
 if __name__ == '__main__':
-    #find_names()
-    find_dependensis()
+    # find_names()
+    manager = ParamsManager()
+    manager.find_dependencies()
     print("Все входные параметры")
     print(all_inputs)
 
-    # print(get_all_input_params_for_out('дальность_обнаружения'))
-    # print(get_all_output_params_for_input('КУ_антенны'))
     while True:
         param = input("Введите входной параметр чтобы узнать что можно посчитать: \n")
         using_params.add(param)
-        get_info_about_input_param(param)
+        manager.get_info_about_input_param(param)

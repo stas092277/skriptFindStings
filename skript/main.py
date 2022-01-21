2da2adcb1ee68e0a531542c9615eb0d0433b13b3
Stanislav Chebikin <47776095+stas092277@users.noreply.github.com>
2022-01-21 13:11:20 +0400

    Refactoring of class, add new method
    Add new method for output param to get needed inputs

--- skript/main.py
+++ skript/main.py
@@ -26,26 +26,40 @@ class ParamsManager:
             for tmp in ins:
                 self.all_inputs.add(tmp)
 
-    def get_all_input_params_for_out(self, name_out_param: str):
+    def add_new_using_param(self, name_input_param):
+        if name_input_param in self.all_inputs:
+            self.using_params.add(name_input_param)
+            print(f'Параметр "{name_input_param}" добавлен в используемые.')
+        else:
+            raise ValueError("Нет такого параметра")
+
+    def get_missing_input_params_for_out(self, name_out_param: str):
         return [param for param in self.dependencies[name_out_param] if param not in self.using_params]
 
-    def get_all_output_params_for_input(self, name_int_param: str):
+    def get_all_output_params_for_input(self, name_input_param: str):
         ans = []
         for key in self.dependencies.keys():
-            if name_int_param in self.dependencies[key]:
+            if name_input_param in self.dependencies[key]:
                 ans.append(key)
         return ans
 
-    def get_info_about_input_param(self, name_int_param: str):
-        if name_int_param in self.all_inputs:
-
+    def get_info_about_inputs_params(self):
+        outputs_for_all_using = set()
+        for name_int_param in self.using_params:
             out_puts = self.get_all_output_params_for_input(name_int_param)
             for out_put in out_puts:
-                needed_input = self.get_all_input_params_for_out(out_put)
-                if len(needed_input) == 0:
-                    print(f'-{out_put}.')
-                else:
-                    print(f'-{out_put}. Не хватает: {self.get_all_input_params_for_out(out_put)}')
+                outputs_for_all_using.add(out_put)
+
+        for out_put in outputs_for_all_using:
+            needed_input = self.get_missing_input_params_for_out(out_put)
+            if len(needed_input) == 0:
+                print(f'-{out_put}.')
+            else:
+                print(f'-{out_put}. Не хватает: {needed_input}')
+
+    def get_info_about_output_param(self, name_out_param: str):
+        if name_out_param in self.dependencies:
+            print(f'-{name_out_param}. Для рассчета нужны: {self.dependencies[name_out_param]}')
         else:
             raise ValueError("Нет такого параметра")
 
@@ -86,10 +100,31 @@ if __name__ == '__main__':
     # find_names()
     manager = ParamsManager()
     manager.find_dependencies()
-    print("Все входные параметры")
-    print(manager.all_inputs)
-
+    print("Введите число чтобы вызвать команду:\n" +
+          "1 - Список всех входных параметров\n" +
+          "2 - Список всех выходных параметров\n" +
+          "3 - Добавить в используемые новый входной параметр\n" +
+          "4 - Список всех используемых параметров\n" +
+          "5 - Узнать что можно вычислить с используемых параметров и каких еще не хватает\n" +
+          "6 - Узнать какие параметры нужны для рассчета выбранного выходного параметра\n"
+          )
     while True:
-        param = input("Введите входной параметр чтобы узнать что можно посчитать: \n")
-        manager.using_params.add(param)
-        manager.get_info_about_input_param(param)
+        command = int(input("Введите номер команды: \n"))
+        if command == 1:
+            print("Все входные параметры:")
+            print(manager.all_inputs)
+        if command == 2:
+            print("Все выходные параметры:")
+            print(list(manager.dependencies.keys()))
+        if command == 3:
+            param = input("Введите входной параметр чтобы добавить его в используемые: \n")
+            manager.add_new_using_param(param)
+        if command == 4:
+            print("Все используемые параметры:")
+            print(manager.using_params)
+        if command == 5:
+            manager.get_info_about_inputs_params()
+        if command == 6:
+            param = input("Введите выходной параметр чтобы узнать что нужно для его расчета: \n")
+            manager.get_info_about_output_param(param)
+
